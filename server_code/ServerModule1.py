b4aef2bd4c7c6729059b6f22d73c7a58dc9f4978
xdjveugs7@proton.me <xdjveugs7@proton.me>
2024-05-15 00:04:54 +0000

    Edited settings, ServerModule1, EntryView and EntryEdit

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -8,7 +8,7 @@ from datetime import datetime
 @anvil.server.callable
 def add_entry(entry_dict):
   app_tables.entries.add_row(
-    created=datetime.now(),
+    datetime=datetime.now(),
     **entry_dict
   )
 
@@ -16,14 +16,13 @@ def add_entry(entry_dict):
 def get_entries():
   # Get a list of entries from the Data Table, sorted by 'created' column, in descending order
   return app_tables.entries.search(
-    tables.order_by("created", ascending=False)
+    tables.order_by("datetime", ascending=False)
   )
 
 @anvil.server.callable
 def update_entry(entry, entry_dict):
   # check that the entry given is really a row in the ‘entries’ table
   if app_tables.entries.has_row(entry):
-    entry_dict['updated'] = datetime.now()
     entry.update(**entry_dict)
   else:
     raise Exception("Entry does not exist")
